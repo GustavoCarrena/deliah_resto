60aa56743c702c585e14c37977e8b27e6425fc84
Gustavo Carrena <gcarre@magyp.ar>
2021-09-07 15:55:27 -0300

    prueba clone

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
 const globalMiddlewares = require('./midllewares/global_middlewares/global_middlewares');
+const { userLogin } = require('./source/controllers/user/userLogin.js');
 
 //GLOBAL MIDDLEWARES
 globalMiddlewares(app);
@@ -14,4 +15,6 @@ routes(app);
 
 //PORT LISTEN
 app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`));
+
+// prueba del pull
 
